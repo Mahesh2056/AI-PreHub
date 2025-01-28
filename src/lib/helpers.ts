3f20f2c4d971438b184a71a4ec21d182e92789cf
mahesh2056 <maheschy2323@gmail.com>
2025-01-28 21:13:33 +0530

    Changes:Header Component Completed

---
+++ src/lib/helpers.ts
@@ -0,0 +1,22 @@
+export const MainRoutes = [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Contact Us",
+      href: "/contact",
+    },
+    {
+      label: "About Us",
+      href: "/about",
+    },
+    {
+      label: "Services",
+      href: "/services",
+    },
+    {
+      label: "Take An Interview",
+      href: "/take",
+    },
+  ];
